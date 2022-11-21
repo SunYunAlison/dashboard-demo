cf623de5228e5222efb779e48d7ea18d552533f1
Sun Yun <95465598+SunYunAlison@users.noreply.github.com>
2022-11-21 17:56:13 +0800

    Delete dashboard-demo directory

--- dashboard-demo/src/app/dashboard-sum/model.ts
+++
@@ -1,7 +0,0 @@
-import { SensorInfo } from "../dashboard/model";
-export class MachineInfo {
-    machineId:string ='';
-    machineName:string ='';
-    sensorInfo:SensorInfo[] = [];
-    updateTime:string ='';
-}
